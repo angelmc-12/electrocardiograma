39da0721ea79a265caba3ee26c5222b92624acfe
angelmc-12 <angelmaytacoaguila@gmail.com>
2025-09-06 08:24:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -268,13 +268,13 @@ elif selected == "📊 Explorador":
         fig = go.Figure()
         fig.add_trace(go.Scattergl(
             x=t_win, y=y_win, mode="lines", name=f"{lead_name}",
-            line=dict(width=1.2),
+            line=dict(width=1.2, color="black"),
             hovertemplate="t=%{x:.3f} s<br>V=%{y:.3f} mV<extra></extra>"
         ))
         if r_win.size > 0:
             fig.add_trace(go.Scattergl(
                 x=t_win[r_win], y=y_win[r_win], mode="markers", name="Picos R",
-                marker=dict(size=7, symbol="diamond"),
+                marker=dict(size=7, symbol="diamond", color="limegreen"),
                 hovertemplate="R @ %{x:.3f} s<extra></extra>"
             ))
 
@@ -282,6 +282,8 @@ elif selected == "📊 Explorador":
             fig.update_layout(shapes=apply_ekg_grid_shapes(
                 x0=0, x1=seconds, y0=y0, y1=y1,
                 x_minor=0.04, x_major=0.20, y_minor=0.1, y_major=0.5,
+                minor_color="rgba(255,204,204,0.5)",  # rojo suave + transparencia
+                major_color="rgba(255,153,153,0.8)",
                 minor_color="#ffcccc", major_color="#ff9999", minor_w=0.5, major_w=1.0
             ))
 
